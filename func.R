3dd3e8bd2f91063ebf77e85ad4cce08345d09341
Alexey Sergushichev <alsergbox@gmail.com>
2017-07-04 18:09:17 +0300

    remove unused functions

--- func.R
+++
@@ -1,4 +0,0 @@
-savePathways <- function(pathways, fileName) {
-    res <- sapply(names(pathways),function(x) paste(x,paste(pathways[[x]],collapse='\t'), collapse = '\t'))
-    lapply(res, write, fileName, append=TRUE)
-}
